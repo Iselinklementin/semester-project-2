ff81cdf359d54a8187ff7dd8db9340377c0eeddf
Iselinklementin <69785627+Iselinklementin@users.noreply.github.com>
2021-11-24 16:21:46 +0100

    finished index - without search/filter

--- js/index.js
+++ js/index.js
@@ -2,8 +2,15 @@ import toggleSidebar from "./layout/nav.js";
 toggleSidebar();
 
 const featuredProducts = document.querySelector(".featured-products-container");
+const herobanner = document.querySelector(".hero-banner");
 
-(async function getProducts() {
+(async function fetchBanner() {
+  const response = await fetch("https://iselin-sp2-api.herokuapp.com/home");
+  const result = await response.json();
+  herobanner.src = `${result.hero_banner.url}`;
+})();
+
+(async function fetchProducts() {
   const response = await fetch("https://iselin-sp2-api.herokuapp.com/products");
   const result = await response.json();
   console.log(result);
@@ -20,36 +27,13 @@ const featuredProducts = document.querySelector(".featured-products-container");
 
                                       <div class="featured-main-info">
                                       <h2 class="card-title">${product.title}</h2>
-                                      <p class="card-price">${product.price} $</p>
-                                      </div>
+                                      <p class="card-price">${product.price}$</p>
+                                      
                                       <p class="card-text">${product.description}</p>
+                                      </div>
                                       
                                       </a> `;
       });
     }
   });
 })();
-
-// const featured = document.querySelector(".featured-container-test");
-
-// (async function getStrapi() {
-//   const response = await fetch("https://iselin-sp2-api.herokuapp.com/products");
-//   const result = await response.json();
-//   console.log(result);
-
-//   result.forEach(product => {
-//     const images = product.image;
-//     console.log(product);
-
-//     if (product.featured) {
-//       images.forEach(img => {
-//         featured.innerHTML += `<div class="container border">
-//                             <h1>${product.title}</h1>
-//                             <p>${product.description}</p>
-//                             <b>${product.price} $</b>
-//                             <img src="${img.url}" class="" style="width:10rem; "alt="..." />
-//                             </div>`;
-//       });
-//     }
-//   });
-// })();
