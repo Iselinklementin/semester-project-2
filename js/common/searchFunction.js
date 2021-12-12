ff3ea9e2dbd8730602197169abd617b4286a3dd1
Iselinklementin <69785627+Iselinklementin@users.noreply.github.com>
2021-12-12 13:06:59 +0100

    fixed product search

--- js/common/searchFunction.js
+++ js/common/searchFunction.js
@@ -1,23 +1,28 @@
 import { createHtml } from "./createHtml.js";
 import { emptyResult } from "../components/emptyResult.js";
+import { productContainer } from "../components/elements.js";
 
 export function searchFunction(products) {
   const searchInput = document.querySelector(".search-product-page");
-  searchInput.onkeyup = event => {
+  searchInput.onkeyup = (event) => {
     const searchValue = event.target.value.trim();
-    const searchTitle = products.filter(item => {
+    const searchTitle = products.filter((item) => {
       // Tar vekk span så produktet kan søkes på
       let titleStr = item.title;
       let title = titleStr.replace("<span>", "");
-      if (
-        title.toLowerCase().includes(searchValue) ||
-        item.description.toLowerCase().includes(searchValue)
-      ) {
+      if (title.toLowerCase().includes(searchValue) || item.description.toLowerCase().includes(searchValue)) {
         return true;
       }
     });
 
+    if (searchTitle.length <= 2) {
+      productContainer.classList = `row row-cols-2 row-cols-md-2 g-4 row-cols-lg-2 results product-container`;
+    } else {
+      productContainer.classList = `row row-cols-2 row-cols-md-2 g-4 row-cols-lg-4 results product-container`;
+    }
+
     createHtml(searchTitle);
+
     emptyResult();
   };
 }
