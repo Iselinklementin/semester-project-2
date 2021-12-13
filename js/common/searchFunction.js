4e759a9188eed464621ae0a5c784fae74d84a9f4
Iselinklementin <69785627+Iselinklementin@users.noreply.github.com>
2021-12-13 13:53:17 +0100

    Update searchFunction.js

--- js/common/searchFunction.js
+++ js/common/searchFunction.js
@@ -4,25 +4,25 @@ import { productContainer } from "../components/elements.js";
 
 export function searchFunction(products) {
   const searchInput = document.querySelector(".search-product-page");
+
   searchInput.oninput = (event) => {
     const searchValue = event.target.value.trim();
-    const searchTitle = products.filter((item) => {
+    const filterProducts = products.filter((item) => {
       // remove span from the title so the search works like it should
-      let titleStr = item.title;
-      let title = titleStr.replace("<span>", "");
+      let title = item.title.replace("<span>", "").replace("</span>", "");
       if (title.toLowerCase().includes(searchValue) || item.description.toLowerCase().includes(searchValue)) {
         return true;
       }
     });
 
     // If its two or less in searchresults, columns is set to 2 instead of 4.
-    if (searchTitle.length <= 2) {
+    if (filterProducts.length <= 2) {
       productContainer.classList = `row row-cols-2 row-cols-md-2 g-4 row-cols-lg-2 results product-container`;
     } else {
       productContainer.classList = `row row-cols-2 row-cols-md-2 g-4 row-cols-lg-4 results product-container`;
     }
 
-    createProductCards(searchTitle);
+    createProductCards(filterProducts);
     emptyResult();
   };
 }
