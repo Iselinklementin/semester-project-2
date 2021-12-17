165c3c0762e25537ab29bec57ce92643b491210c
Iselinklementin <69785627+Iselinklementin@users.noreply.github.com>
2021-12-17 08:28:10 +0100

    Update searchFunction.js

--- js/common/searchFunction.js
+++ js/common/searchFunction.js
@@ -5,25 +5,29 @@ import { productContainer } from "../components/elements.js";
 export function searchFunction(products) {
   const searchInput = document.querySelector(".search-product-page");
 
-  searchInput.oninput = event => {
+  searchInput.oninput = (event) => {
     const searchValue = event.target.value.trim();
-    const filterProducts = products.filter(item => {
-      // remove span from the title
-      let title = item.title.replace("<span>", "").replace("</span>", "");
-      if (
-        title.toLowerCase().includes(searchValue) ||
-        item.description.toLowerCase().includes(searchValue)
-      ) {
+    const filterProducts = products.filter((item) => {
+      console.log(searchValue);
+
+      if (item.title.toLowerCase().includes(searchValue)) {
         return true;
       }
+
+      // denne fungerer
+      // remove span from the title
+      // let title = item.title.replace("<span>", "").replace("</span>", "");
+      // if (title.toLowerCase().includes(searchValue) || item.description.toLowerCase().includes(searchValue)) {
+      //   return true;
+      // }
     });
 
     // If its two or less in searchresults, columns is set to 2 instead of 4.
-    if (filterProducts.length <= 2) {
-      productContainer.classList = `row row-cols-2 row-cols-md-2 g-4 row-cols-lg-2 results product-container`;
-    } else {
-      productContainer.classList = `row row-cols-2 row-cols-md-2 g-4 row-cols-lg-4 results product-container`;
-    }
+    // if (filterProducts.length <= 2) {
+    //   productContainer.classList = `row row-cols-2 row-cols-md-2 g-4 row-cols-lg-2 results product-container`;
+    // } else {
+    //   productContainer.classList = `row row-cols-2 row-cols-md-2 g-4 row-cols-lg-4 results product-container`;
+    // }
 
     createProductCards(filterProducts);
     emptyResult();
