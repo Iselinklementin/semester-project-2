50b77e8755083e74070c175989c7f710916c018b
Iselinklementin <69785627+Iselinklementin@users.noreply.github.com>
2021-12-17 09:22:10 +0100

    Update searchFunction.js

--- js/common/searchFunction.js
+++ js/common/searchFunction.js
@@ -6,33 +6,25 @@ export function searchFunction(products) {
   const searchInput = document.querySelector(".search-product-page");
 
   searchInput.oninput = (event) => {
-    const searchValue = event.target.value.trim();
+    const searchValue = event.target.value.trim().toLowerCase();
 
     const filterProducts = products.filter((item) => {
-      const titleProduct = item.title.toLowerCase();
+      // remove span from the title
+      let title = item.title.replace("<span>", "").replace("</span>", "").toLowerCase();
       const descProduct = item.description.toLowerCase();
-
-      if (titleProduct.includes(searchValue)) {
+      if (descProduct.includes(searchValue) || title.toLowerCase().includes(searchValue)) {
         return true;
       }
-      //
-      // remove span from the title
-      // let title = item.title.replace("<span>", "").replace("</span>", "");
-      // if (title.toLowerCase().includes(searchValue) || item.description.toLowerCase().includes(searchValue)) {
-      //   return true;
-      // }
     });
 
-    // // If its two or less in searchresults, columns is set to 2 instead of 4.
-    // if (filterProducts.length <= 2) {
-    //   productContainer.classList = `row row-cols-2 row-cols-md-2 g-4 row-cols-lg-2 results product-container`;
-    // } else {
-    //   productContainer.classList = `row row-cols-2 row-cols-md-2 g-4 row-cols-lg-4 results product-container`;
-    // }
-
-    if (filterProducts) {
-      createProductCards(filterProducts);
+    // If its two or less in searchresults, columns is set to 2 instead of 4.
+    if (filterProducts.length <= 2) {
+      productContainer.classList = `row row-cols-2 row-cols-md-2 g-4 row-cols-lg-2 results product-container`;
+    } else {
+      productContainer.classList = `row row-cols-2 row-cols-md-2 g-4 row-cols-lg-4 results product-container`;
     }
+
+    createProductCards(filterProducts);
     emptyResult();
   };
 }
