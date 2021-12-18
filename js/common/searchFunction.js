3e7ea3bd221da808960261f567c05d429f720e08
Iselinklementin <69785627+Iselinklementin@users.noreply.github.com>
2021-12-18 13:35:06 +0100

    fixed products

--- js/common/searchFunction.js
+++ js/common/searchFunction.js
@@ -4,27 +4,42 @@ import { productContainer } from "../components/elements.js";
 
 export function searchFunction(products) {
   const searchInput = document.querySelector(".search-product-page");
+  const filterBtn = document.querySelectorAll(".filter-btn");
 
-  searchInput.oninput = (event) => {
+  searchInput.oninput = event => {
     const searchValue = event.target.value.trim().toLowerCase();
+    const findChildrenClass = [...filterBtn];
+    const hasClass = findChildrenClass.filter(child => child.classList.contains("active-filter"));
 
-    const filterProducts = products.filter((item) => {
+    const filterProducts = products.filter(product => {
       // remove span from the title
-      let title = item.title.replace("<span>", "").replace("</span>", "").toLowerCase();
-      const descProduct = item.description.toLowerCase();
+      let title = product.title.replace("<span>", "").replace("</span>", "").toLowerCase();
+      const descProduct = product.description.toLowerCase();
+
       if (descProduct.includes(searchValue) || title.includes(searchValue)) {
         return true;
       }
     });
 
+    // If its filtered on volume first, and then on input
+    let filterVolume = filterProducts.filter(product => {
+      if (hasClass[0].value === product.volume) {
+        return true;
+      }
+    });
+
     // If its two or less in searchresults, columns is set to 2 instead of 4.
-    if (filterProducts.length <= 2) {
-      productContainer.classList = `row row-cols-2 row-cols-md-2 g-4 row-cols-lg-2 results product-container`;
+    if (filterProducts.length === 1 || filterVolume.length === 1) {
+      productContainer.classList = `p-0 row row-cols-1 g-4 row-cols-lg-1 results product-container`;
     } else {
-      productContainer.classList = `row row-cols-2 row-cols-md-2 g-4 row-cols-lg-4 results product-container`;
+      productContainer.classList = `container p-0 row row-cols-2 g-4 row-cols-lg-4 results product-container`;
     }
 
-    createProductCards(filterProducts);
+    if (filterVolume.length) {
+      createProductCards(filterVolume);
+    } else {
+      createProductCards(filterProducts);
+    }
     emptyResult();
   };
 }
